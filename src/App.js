f7d8b9930724ac51d647baab537ea0c6c6eeda48
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-06-19 22:25:46 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  const [countries,setCountries] = useState([]);
-  useEffect(()=>{
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
     fetch("https://xcountries-backend.azurewebsites.net/all")
-    .then((response)=> response.json())
-    .then((data)=> setCountries(data))
-    .catch((error)=> console.error(error));
-  },[])
-  const containerStyle ={
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const containerStyle = {
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
-    alignItems :"center",
-    height: "100vh"
-  }
-  const countryStyle ={
+    alignItems: "center",
+    height: "100vh",
+  };
+
+  const countryStyle = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
@@ -26,22 +29,21 @@ function App() {
     border: "1px solid #ccc",
     borderRadius: "10px",
     width: "200px",
-    
-    
+  };
 
-  }
-  const flagStyle ={
+  const flagStyle = {
     width: "100px",
-    height: "100px"
-  }
+    height: "100px",
+  };
+
   return (
     <div style={containerStyle}>
-     {countries.map((country)=>(
-      <div key={country.cca3} style={countryStyle} >
-        <img src= {country.flag} alt={country.name} style={flagStyle}/>
-        <h2>{country.name}</h2>
-      </div>
-     ))}
+      {countries.map((country) => (
+        <div key={country.cca3} style={countryStyle}>
+          <img src={country.flag} alt={country.name} style={flagStyle} />
+          <h2>{country.name}</h2>
+        </div>
+      ))}
     </div>
   );
 }
