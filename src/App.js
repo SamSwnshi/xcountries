20a74acc731294abfb4e841bc59b6badc9b050d7
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-06-19 21:20:21 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React,{useState,useEffect} from "react";
+import './App.css';
+
+function App() {
+  const [countries,setCountries] = useState([]);
+  useEffect(()=>{
+    fetch("https://xcountries-backend.azurewebsites.net/all")
+    .then((response)=> response.json())
+    .then((data)=> setCountries(data))
+    .catch((error)=> console.error(error));
+  },[])
+  const containerStyle ={
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems :"center",
+    height: "100vh"
+  }
+  const countryStyle ={
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "10px",
+    padding: "10px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    width: "200px",
+    
+    
+
+  }
+  const flagStyle ={
+    width: "100px",
+    height: "100px"
+  }
+  return (
+    <div style={containerStyle}>
+     {countries.map((country)=>(
+      <div key={country.cca3} style={countryStyle} >
+        <img src= {country.flag} alt={country.name.common} style={flagStyle}/>
+        <h2>{country.name}</h2>
+      </div>
+     ))}
+    </div>
+  );
+}
+
+export default App;
